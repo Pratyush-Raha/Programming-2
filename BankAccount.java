7078d15fe537b168655ab08b1dd30f44bb4d9fb0
Pratyush Raha <pratyushraha32@gmail.com>
2025-06-19 01:05:44 +0600

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,33 @@
+public class BankAccount {
+    String accountNumber;
+    String accountHolderName;
+    double balance;
+
+    BankAccount(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Invalid or insufficient funds.");
+        }
+    }
+
+    double checkBalance() {
+        return balance;
+    }
+}
